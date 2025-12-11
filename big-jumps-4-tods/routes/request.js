5ad74c3e2ec8b63971f5459c84366d11aae31f6d
richard williams <richard.williams5697@yahoo.com>
2025-12-11 10:56:49 -0500

    created db.js

--- big-jumps-4-tods/routes/request.js
+++ big-jumps-4-tods/routes/request.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../knexfile');
-
 console.log('conneting requests');
 
 router.post('/', async (req, res, next) => {
