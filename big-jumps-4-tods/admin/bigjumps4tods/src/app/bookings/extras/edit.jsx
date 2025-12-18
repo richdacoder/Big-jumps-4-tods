0ac9561ba405bfcb9c54e221b7da7d50917d8c55
richard williams <richard.williams5697@yahoo.com>
2025-12-18 11:15:06 -0500

    added fetch put in edit page

--- big-jumps-4-tods/admin/bigjumps4tods/src/app/bookings/extras/edit.jsx
+++ big-jumps-4-tods/admin/bigjumps4tods/src/app/bookings/extras/edit.jsx
@@ -1,10 +1,35 @@
 "use clients"
+import { useEffect, useState } from "react";
 
 export default function EditBookings({booking}){
 //fetch get bookings
 console.log('check for booking', booking)
 //fetch edit or put
+useEffect(() => {
+  const edit = async () => {
+    try {
+      const res = await fetch('http://localhost:3002/api/bookings', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ modifystring: "yourValueHere" })
+      });
+
+      if (!res.ok) throw new Error('Failed to update booking');
+
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  edit();
+}, []); // run once on mount
+
 //fetch delete
+
 //make submit form with input and get
 //make iteration of booking in placeholder
 //make class call hide on pop up
