786f26ac018fb8f500447044d34e39d5b46a5b39
richard williams <richard.williams5697@yahoo.com>
2025-12-20 03:17:21 -0500

    Stop tracking build artifacts in .next folder

--- big-jumps-4-tods/.next/dev/static/chunks/app_book_page_jsx_b6a0862f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_styles_book_4699b9ff.css",
-  "static/chunks/app_book_5c722908._.js"
-],
-    source: "dynamic"
-});
