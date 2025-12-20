ecc652d6e8b3f96666eff1dfb6ded34d18c2847e
richard williams <richard.williams5697@yahoo.com>
2025-12-20 02:41:52 -0500

    commit again

--- big-jumps-4-tods/app/confirmation/page.jsx
+++ big-jumps-4-tods/app/confirmation/page.jsx
@@ -8,7 +8,7 @@ function ConfirmRequest() {
   - on click
   - query redirect page and sends user info through
   - page renders confirmation
-  - confirmation page to saying your "thsnk you first and last name request have been sent for month day year for start to end time we will get back at you at ou shortly letting you know if its available or not
+  - confirmation page to saying your "thsnk you first and last name request have been sent for month day year forgco  start to end time we will get back at you at ou shortly letting you know if its available or not
 
 
 
