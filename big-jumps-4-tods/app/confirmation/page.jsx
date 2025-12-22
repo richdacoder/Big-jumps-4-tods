9963d5e89f93b138717ea96b5f64d9947cf1d898
richard williams <richard.williams5697@yahoo.com>
2025-12-22 17:10:50 -0500

    merged

---
+++ big-jumps-4-tods/app/confirmation/page.jsx
@@ -0,0 +1,42 @@
+"use client";
+import { useState } from "react";
+
+function ConfirmRequest() {
+  /*
+  - user clicks book now
+  - make partial for confirmation
+  - on click
+  - query redirect page and sends user info through
+  - page renders confirmation
+  - confirmation page to saying your "thsnk you first and last name request have been sent for month day year forgco  start to end time we will get back at you at ou shortly letting you know if its available or not
+
+
+
+
+
+
+  */
+
+
+  return (
+    <div>Request confirmed!</div>
+  );
+}
+
+export default ConfirmRequest;
+
+
+/*
+  - user clicks book now
+  - make partial for confirmation
+  - on click
+  - query redirect page and sends user info through
+  - page renders confirmation
+  - confirmation page to saying your "thsnk you first and last name request have been sent for month day year for start to end time we will get back at you at ou shortly letting you know if its available or not
+
+
+
+
+
+
+  */
