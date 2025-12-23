39851a351c4cc4f4d600ca503a34f28d1b3ad7f0
richard williams <richard.williams5697@yahoo.com>
2025-12-23 13:18:04 -0500

    Ignore Next.js build output (.next)

--- big-jumps-4-tods/.next/dev/static/chunks/app_book_page_jsx_b6a0862f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_styles_book_4699b9ff.css",
-  "static/chunks/_9f663e40._.js"
-],
-    source: "dynamic"
-});
