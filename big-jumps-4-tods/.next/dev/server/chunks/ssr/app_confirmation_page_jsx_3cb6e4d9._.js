a1caa1c92fbc56810d50134e5eed3e218774e8a8
richard williams <richard.williams5697@yahoo.com>
2025-12-23 00:48:12 -0500

    no git ignore

---
+++ big-jumps-4-tods/.next/dev/server/chunks/ssr/app_confirmation_page_jsx_3cb6e4d9._.js
@@ -0,0 +1,53 @@
+module.exports = [
+"[project]/app/confirmation/page.jsx [app-ssr] (ecmascript)", ((__turbopack_context__) => {
+"use strict";
+
+__turbopack_context__.s([
+    "default",
+    ()=>__TURBOPACK__default__export__
+]);
+var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/server/route-modules/app-page/vendored/ssr/react-jsx-dev-runtime.js [app-ssr] (ecmascript)");
+var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/server/route-modules/app-page/vendored/ssr/react.js [app-ssr] (ecmascript)");
+"use client";
+;
+;
+function ConfirmRequest() {
+    /*
+  - user clicks book now
+  - make partial for confirmation
+  - on click
+  - query redirect page and sends user info through
+  - page renders confirmation
+  - confirmation page to saying your "thsnk you first and last name request have been sent for month day year forgco  start to end time we will get back at you at ou shortly letting you know if its available or not
+
+
+
+
+
+
+  */ return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
+        children: "Request confirmed!"
+    }, void 0, false, {
+        fileName: "[project]/app/confirmation/page.jsx",
+        lineNumber: 22,
+        columnNumber: 5
+    }, this);
+}
+const __TURBOPACK__default__export__ = ConfirmRequest;
+ /*
+  - user clicks book now
+  - make partial for confirmation
+  - on click
+  - query redirect page and sends user info through
+  - page renders confirmation
+  - confirmation page to saying your "thsnk you first and last name request have been sent for month day year for start to end time we will get back at you at ou shortly letting you know if its available or not
+
+
+
+
+
+
+  */ }),
+];
+
+//# sourceMappingURL=app_confirmation_page_jsx_3cb6e4d9._.js.map
---
+++ big-jumps-4-tods/.next/dev/static/chunks/app_confirmation_page_jsx_b6a0862f._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/app_confirmation_page_jsx_9e898dac._.js"
+],
+    source: "dynamic"
+});
