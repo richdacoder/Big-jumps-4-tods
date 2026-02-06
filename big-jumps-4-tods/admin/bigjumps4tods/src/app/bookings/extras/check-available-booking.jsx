bce5010adc1d898b18e8b7a5530b3a3511391dff
richard williams <richard.williams5697@yahoo.com>
2026-02-06 11:53:23 -0500

    created callback function for check availabilty

--- big-jumps-4-tods/admin/bigjumps4tods/src/app/bookings/extras/check-available-booking.jsx
+++ big-jumps-4-tods/admin/bigjumps4tods/src/app/bookings/extras/check-available-booking.jsx
@@ -1,6 +1,11 @@
 "use client";
 /*
 - click button
+- create function in edit.jsx (checkBooking)
+- throw set state in that function
+- if its true ->update button if false-> contact user
+- pass date(state), starttime(state) and endtime(state) and the function (checkBooking) to this component
+
 - go inside database or display it
 - compare desired date and start time and end time to all in database
 - if available pop up upgrade button]
@@ -9,7 +14,10 @@
 
 
 */
-  const CheckAvailableBooking = async () => {
+
+  const CheckAvailableBooking = ({date, startTime, endTime, checkAvailability}) => {
+
+  const checkExistingBooking = async () => {
     const res = await fetch('http://localhost:3002/api/bookings');
     const bookings = await res.json();
     const [Sdate, bookingStartTime] = bookings.party_start_time.split('T');
@@ -29,5 +37,6 @@
     })
 
 };
+  }
 
 export default CheckAvailableBooking;
