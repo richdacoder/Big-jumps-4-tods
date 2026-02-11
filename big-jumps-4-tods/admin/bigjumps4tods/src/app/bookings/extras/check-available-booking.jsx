b4351b44d14377260e346bc57b14a500672d475a
richard williams <richard.williams5697@yahoo.com>
2026-02-11 14:47:49 -0500

    changed format timestamp to use constructors for new date

--- big-jumps-4-tods/admin/bigjumps4tods/src/app/bookings/extras/check-available-booking.jsx
+++ big-jumps-4-tods/admin/bigjumps4tods/src/app/bookings/extras/check-available-booking.jsx
@@ -12,7 +12,7 @@
 - if available pop up upgrade button]
 - if not contact user
 
-
+ 56 | gosto      | papa      | 92903940393   | richardwilliams5697@yahoo.com  | 2026-02-12 | yse      | 33 yo ave     |               | 2026-02-12 14:19:00-05 | 2026-02-12 20:21:00-05 | how                           |         |          | 2026-01-27 17:20:39.721203-05 | 2026-01-27 17:20:39.721203-05
 
 
 */
@@ -27,41 +27,47 @@
       }
     )
     const toUTC = (dateString, timeString) => {
+
+      // const newDate = new Date(`${dateString}T${timeString}`);
+      // console.log('new date', newDate )
+      // const [date, time] = newDate.toISOString().split('T');
+      // console.log('date time construct', date, time);
       const [y, m, d] =  dateString.split('-').map(Number);
       const [hours, mins] = timeString.split(':').map(Number);
 
       console.log('before utc',  hours, mins,  'split', m, d, y);
 
-      return new Date(Date.UTC(y, m - 1, d, hours, mins))
+      return new Date(Date.UTC(y, m - 1, d , hours, mins));
     }
+
+    const fakeDate = '2026-2-10';
+    const fakeStartTime = '13:00';
+    const fakeEndTime = '15:04';
+    const testSTime =  toUTC(fakeDate, fakeStartTime );
+    const testETime =  toUTC(fakeDate, fakeEndTime );
+
 
     const desiredStartTime = toUTC(partyDate, startTime);
     const desiredEndTime = toUTC(partyDate, endTime);
-    console.log('desired', desiredStartTime, 'desired end', desiredEndTime);
+    console.log(
+      Intl.DateTimeFormat().resolvedOptions()
+    );
+
+    console.log('desired get utc', desiredStartTime, 'desired end', desiredEndTime);
 
     try{
     const res = await fetch('http://localhost:3002/api/bookings');
     const bookings = await res.json();
 
     const isOverLap = bookings.some(b => {
-      // const desiredStartTime = new Date(Date.UTC(`${partyDate}T${ startTime}`));
-      // const desiredEndTime = new Date(`${partyDate}T${endTime}`);
-      const existingStartTime = new Date(b.party_start_time);
-      const existingEndTime = new Date(b.party_end_time);
+      const existingStartTime = b.party_start_time;
+      const existingEndTime = b.party_end_time;
+      console.log(
+        Intl.DateTimeFormat().resolvedOptions()
+      );
 
-      // console.log('desired', {
-      //   'start': desiredStartTime,
-      //   'end': desiredEndTime,
-      //   'date': partyDate
-      // });
+      console.log('first name:', b.first_name, 'existing start', existingStartTime, 'existing end', existingEndTime);
 
-      // console.log('existing', {
-      //   'start': existingStartTime,
-      //   'end': existingEndTime,
-      //   'start no date': b.party_start_time,
-      //   'end no date': b.party_end_time
-
-      // })
 
       // add day plus one day
       // or convert desired time to utc
