03629ae3c8a107d60b439a2d74dad8dcc8ef83f2
richard williams <richard.williams5697@yahoo.com>
2026-02-13 22:07:49 -0500

    added express() to contact user router

--- big-jumps-4-tods/routes/contact-user.js
+++ big-jumps-4-tods/routes/contact-user.js
@@ -1,5 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { send } = require("../services/contact-user.js");
+
+
 
 function contactUserRouter(){
+
   console.log('contact user');
 
 }
