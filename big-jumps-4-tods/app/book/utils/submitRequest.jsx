990872472272f5c2d3850c43da3ca53d39adc52c
richard williams <richard.williams5697@yahoo.com>
2026-03-19 09:42:23 -0400

    added url test

--- big-jumps-4-tods/app/book/utils/submitRequest.jsx
+++ big-jumps-4-tods/app/book/utils/submitRequest.jsx
@@ -1,6 +1,9 @@
 export const submitRequest = async (formData) => {
   console.log('before fetch', structuredClone(formData));
 
+  // if (formData.url === 'http://localhost:3002/api/request'){
+
+
   const res = await fetch('http://localhost:3002/api/request', {
     method: 'POST',
     headers: {
@@ -8,11 +11,16 @@ export const submitRequest = async (formData) => {
     },
     body: JSON.stringify(formData)
   });
-  console.log('after fetch', structuredClone(formData));
+  console.log('after fetch', structuredClone(formData),
+  window.location.hostname === 'localhost'
+  ? 'http://localhost:3002'
+  : 'https://big-jumps-api.onrender.com'
+  );
   if (!res.ok) {
     const errorData = await res.json();
     throw errorData;
   }
+// }
 return res.json();
 
 };
