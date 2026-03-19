19998314b969fb66f2fbba05552b30554c8009a3
richard williams <richard.williams5697@yahoo.com>
2026-03-19 10:18:06 -0400

    added more test in  console.log

--- big-jumps-4-tods/app/book/utils/submitRequest.jsx
+++ big-jumps-4-tods/app/book/utils/submitRequest.jsx
@@ -11,7 +11,7 @@ export const submitRequest = async (formData) => {
     },
     body: JSON.stringify(formData)
   });
-  console.log('after fetch', structuredClone(formData), window.location.hostname,
+  console.log('after fetch', structuredClone(formData), window.location.hostname,'working',
   window.location.hostname === 'localhost'
   ? 'http://localhost:3002'
   : 'https://big-jumps-api.onrender.com'
